2b6c5bca882a9c1144a9ce83189354c9c30e61f2
iHTCboy <ihetiancong@gmail.com>
2019-04-07 20:29:43 +0800

    init project

---
+++ iWuBi/iWuBi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iWuBi
+//
+//  Created by HTC on 2019/4/7.
+//  Copyright © 2019 HTC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
